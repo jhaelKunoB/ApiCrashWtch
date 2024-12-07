d55e3703ff88e1741b05022008e752eceab9d2cb
Jhael Kuno Bustos <JhaelKuno345@gmail.com>
2024-12-07 13:16:15 -0400

    tercera modificacion

--- api2.js
+++ api2.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // const { Pool } = require('pg');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const pool = require('./server');
 require('dotenv').config();
 const app = express();
